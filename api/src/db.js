587f4ca51560cc4eef3f8b762a4b4048410e105d
Mariana8819 <mivallejos8819@gmail.com>
2024-08-29 11:47:33 -0300

    please

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+//const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-//const { DATABASE_URL } = process.env;
+const { DATABASE_URL } = process.env;
 //postgresql://postgres:FVQvZzKGhHTaOjxAVEMyzLdbYzeWdDUW@meticulous-empathy.railway.internal:5432/railway
 const sequelize = new Sequelize(
-  // DATABASE_URL,
-   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${ DB_PORT}/${DB_NAME}`,
+   DATABASE_URL,
+  // `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${ DB_PORT}/${DB_NAME}`,
       {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
