7ef528592ce2809368f426226e5792db24895fc8
Mariana8819 <mivallejos8819@gmail.com>
2024-08-29 10:51:29 -0300

    variables de entorno agregadas

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const { DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
   // DATABASE_URL,
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${ DB_PORT}/pokemonii`,
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${ DB_PORT}/${DB_NAME}`,
       {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
